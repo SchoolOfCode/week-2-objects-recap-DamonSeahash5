cb8de71eceba721b32533655db86cb41bfc2af5a
DamonSeahash5 <oliverhopwood5@gmail.com>
2024-12-03 17:43:04 +0000

    Added Task 2 to main.js

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 const myHouse = {
-    stories: "Two",
-    parking: "None",
-    bedrooms: "Two",
-    garden: "One",
+    stories: 2,
+    parking: null,
+    bedrooms: 2,
+    garden: true,
     energyEfficiencyRating: "D",
 }
 
 console.log(myHouse)
+
+const myCar = {
+    colour: "red",
+    powersteering: true,
+    seats: 2,
+    mpg: 20,
+}
+
+const myFavouriteFilm = {
+    title: "The Dark Knight",
+    releaseDate: 2008,
+    leadActor: "Christian Bale",
+    }
+
+
+    let person = {
+        name: "Ben",
+        likesChocolate: true,
+      };
+
+console.log(`Does ${person.name} like chocolate? ${person.likesChocolate}`)
+console.log(Object.keys(person))
